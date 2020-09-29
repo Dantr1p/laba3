1ef2eeec88a01ef843a75d2c99165ba83928f1cd
Daniil Orlov <D4nnisimo@yandex.ru>
2020-09-29 19:39:21 +0700

    Добавьте файлы проекта.

---
+++ laba3/laba3.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <conio.h>
+
+using namespace std;
+
+class laptop
+{
+	string model];	//модель ноутбука
+	int RAM_size;// объем оперативной памяти
+	int CPU_frequency; //частота процессора
+	double price;// цена ноутбука
+	int year;// Год производства
+//public:
+	laptop();
+	laptop(string model, int ram, int cpu, double price, int year);
+	~laptop();
+	void set_laptop(string model, int ram, int cpu, double price, int year);
+	void print_laptop();
+};
+ 
+
+
+
+int main()
+{
+	system("color f0");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+}
+
